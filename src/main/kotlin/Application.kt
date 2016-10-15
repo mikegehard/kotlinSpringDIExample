3009ca8131f6d12dde3ae4fc9291d9d989562778
Mike Gehard <mike.gehard@gmail.com>
2016-10-15 10:44:13 -0600

    Hard code depencies

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>) {
-    println("Hello world!")
+    val connection: DatabaseConnection = MySqlConnection()
+    val action: SaveUser = SaveUser(connection)
+
+    action.execute()
+}
+
+class SaveUser(val dbConnection: DatabaseConnection) {
+    fun execute() {
+        dbConnection.execute("insert into users")
+    }
+}
+
+interface DatabaseConnection {
+    fun execute(sql: String)
+}
+
+class MySqlConnection() : DatabaseConnection {
+    override fun execute(sql: String) {
+        println("Executing $sql against MySQL")
+    }
+
 }
